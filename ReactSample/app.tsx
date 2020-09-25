d9f8ab1bb99f374f7af7f6e6b276bdad7a7e643d
Diego Fernando Sanchez <d.sanchez@dxc.com>
2020-09-25 15:02:22 -0500

    Ajustes

--- ReactSample/app.tsx
+++ ReactSample/app.tsx
@@ -3,12 +3,25 @@ declare var require: any
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+import config from './aws-exports';
+import { API } from 'aws-amplify';
+
+
 export class Hello extends React.Component {
+
+    fetchNotes() {
+        
+        //const apiData = API.ApiTestReact.graphql({ query: listNotes });
+        //setNotes(apiData.data.listNotes.items);
+    }
+
     render() {
         return (
             <h1>Welcome to React!!</h1>
         );
     }
 }
+
+
 
 ReactDOM.render(<Hello />, document.getElementById('root'));
--- ReactSample/dist/app-bundle.js
+++ ReactSample/dist/app-bundle.js
@@ -95,32 +95,18 @@
 
 "use strict";
 
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 Object.defineProperty(exports, "__esModule", { value: true });
 var React = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 var ReactDOM = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/index.js");
-var Hello = /** @class */ (function (_super) {
-    __extends(Hello, _super);
-    function Hello() {
-        return _super !== null && _super.apply(this, arguments) || this;
+class Hello extends React.Component {
+    fetchNotes() {
+        //const apiData = API.ApiTestReact.graphql({ query: listNotes });
+        //setNotes(apiData.data.listNotes.items);
     }
-    Hello.prototype.render = function () {
+    render() {
         return (React.createElement("h1", null, "Welcome to React!!"));
-    };
-    return Hello;
-}(React.Component));
+    }
+}
 exports.Hello = Hello;
 ReactDOM.render(React.createElement(Hello, null), document.getElementById('root'));
 
